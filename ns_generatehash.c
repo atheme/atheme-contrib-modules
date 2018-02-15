5a66cff02a9b16e6a9ebc485da556419fd44240e
Aaron Jones <aaronmdjones@gmail.com>
2018-02-15 01:30:57 +0000

    ns_generatehash: use crypt_password() function if building on 7.3

--- ns_generatehash.c
+++ ns_generatehash.c
@@ -36,7 +36,11 @@ static void ns_cmd_generatehash(sourceinfo_t *si, int parc, char *parv[])
 		return;
 	}
 
+#if (CURRENT_ABI_REVISION >= 730000)
+	const char *const hash = crypt_password(key);
+#else
 	const char *const hash = crypt_string(pass, NULL);
+#endif
 
 	if (hash)
 		command_success_string(si, hash, "Hash is: %s", hash);
